1ef1bfbe390290ad1c407000ea4b81316d039f6b
Diego Abanto <dabanto@belatrixsf.com>
2017-09-29 11:45:14 -0500

    CREANDO EL SERVICIO HEROES - CONTROL DEL INSERT, DELETE, UPDATE Y SELECT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //Import
 import { HttpModule } from '@angular/http';
-
+import { FormsModule } from '@angular/forms';
 //Import
 import { APP_ROUTING } from './app.routes';
+//Import Services
+import { HeroesService } from './services/heroes.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -18,9 +20,13 @@ import { HeroeComponent } from './components/heroes/heroe.component';
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//Import
+import { NgForm } from '@angular/forms';
+//Import interface
+import { Heroe } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'app-heroe',
@@ -6,6 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HeroeComponent implements OnInit {
+
+  heroe:Heroe = {
+    nombre:"",
+    bio:"",
+    casa:"Marvel"
+    //Key, no lo tengo, no tengo manera de saberlo
+  }
 
   constructor() { }
 

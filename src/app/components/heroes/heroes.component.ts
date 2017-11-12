8007c7cf8f90befcc6c65a774e7e1f933d6fc49c
diego <diego@diego-lenovo.cpe.tdp.com>
2017-11-11 21:00:05 -0500

    MAQUILLAJE PARA NUESTRA PANTALLA DE HEROES

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -9,27 +9,18 @@ import { HeroesService } from '../../services/heroes.service';
 export class HeroesComponent implements OnInit {
 
   heroes:any[] = [];
+  loading:boolean = true;
 
   constructor(private _heroesService:HeroesService) {
 
     //subscribe me devolvera la data que viene de mi servicio
     this._heroesService.getHeroes()
       .subscribe( data => {
-        console.log(data);
-        //Convertir un objeto en un arreglo
-        /*
-        for(let key$ in data){
-
-          let h = data[key$];
-          h.key$ = key$
-          this.heroes.push( data[key$] );
-        }
-        console.log( this.heroes );
-        */
-
         //Crear un PIPE para trabajar esto
-        this.heroes = data;
-
+        setTimeout( ()=>{
+          this.loading = false;
+          this.heroes = data;
+        }, 2000 )
       })
   }
 
